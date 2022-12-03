f19035350f6b97d30f4413dac882f922b80c1399
Murad <114482946+MuradKuriev@users.noreply.github.com>
2022-12-03 17:34:03 +0300

    Update pz_6_2.py

--- PZ_6/pz_6_2.py
+++ PZ_6/pz_6_2.py
@@ -1,1 +1,7 @@
-
+list1 = (list(map(int, (input('Введите ряд чисел: ')).split())))
+sum = 0
+for i in range(len(list1)-1):
+  if (list1[i] + list1[i+1] > sum):
+    sum = list1[i] + list1[i+1]
+    a = i
+print(f'Сумма {a+1} и {a+2} элементов равна: {sum}')
