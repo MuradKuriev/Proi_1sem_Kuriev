9a8f2fb6b2995e4347976b077687ae3326dce284
Murad <114482946+MuradKuriev@users.noreply.github.com>
2022-12-15 20:51:50 +0300

    Update pz_9.py

--- pz_9/pz_9.py
+++ pz_9/pz_9.py
@@ -1,1 +1,18 @@
-
+def program():
+  dic ={"Cat": "Кот",
+        "House": "Дом",
+        "Car": "Машина",
+        "Hospital": "Больница",
+        "Dog": "Собака",
+        "Carrot": "Морковка",
+        "A monkey": "Обезьяна",
+        "Garden": "Огород",
+        "Spider": "Паук",
+        "Phone": "Телефон"}
+  a = str(input())
+  if a not in dic:
+        print("Неверное значение")
+        program()
+  else:
+        print(dic[a])
+program()
