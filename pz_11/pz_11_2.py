b59e60c46b2b1153dff4f1fbd5dd7852b6a4ed9f
Murad <114482946+MuradKuriev@users.noreply.github.com>
2023-03-18 01:28:07 +0300

    Create pz_11_2.py

---
+++ pz_11/pz_11_2.py
@@ -0,0 +1,19 @@
+# Читаем содержимое файла
+with open('sequence.txt', 'r') as f:
+    content = f.read()
+
+# Выводим содержимое файла и количество пробельных символов
+print(content)
+print(f'Количество пробельных символов: {content.count(" ")}')
+
+# Строим строку в стихотворной форме
+poem = ''
+for line in content.split('\n'):
+    if not line:
+        continue
+    poem += f'{line},\n'
+poem += '*' * 10
+
+# Записываем стихотворение в файл
+with open('poem.txt', 'w') as f:
+    f.write(poem)
