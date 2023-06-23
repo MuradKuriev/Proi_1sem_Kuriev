59e670fdc6c79b5ae2aba0afe205d4aba6fa2212
Murad <114482946+MuradKuriev@users.noreply.github.com>
2023-06-23 19:30:23 +0300

    Create 2.py

---
+++ pz_регуляркам/2.py
@@ -0,0 +1,8 @@
+#Найдите все «слова», написанные капсом (то есть строго заглавными), возможно внутри настоящих слов (аааБББввв):
+
+import re
+text = "АААА аааа АаАаАаАа 123 123 12345 11223344"
+pattern = r'\b[A-Z]+\b'
+
+uppercase_words = re.findall(pattern, text)
+print(uppercase_words)
